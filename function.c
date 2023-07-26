30a194bfdbf957e923a46177d2ad7989155242f2
Gideoncancode <gidonuagbe@gmail.com>
2023-07-25 18:38:38 -0700

    UPDATE

--- function.c
+++ function.c
@@ -18,6 +18,4 @@ int print_char(va_list types, char buffer[], int flags, int width, int precision
 {
 	char c = va_arg(types, int);
 
-	return (handle_write_char(c, buffer, flags, width, precision, size));
-}
-
+	return (handle_write_char(c, buffer, flags, width, precision, size))#include "main.h"
